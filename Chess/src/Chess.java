88403e6212d7c47124fab0623f7c989b4aab7f2b
rl512 <linraymond81@gmail.com>
2020-03-04 12:55:57 -0500

    Modified more illegal moves, queen, pawn, bishop, and rook

--- Chess/src/Chess.java
+++ Chess/src/Chess.java
@@ -316,7 +316,9 @@ public class Chess {
 		Chesspiece newpiece; // new position where the player wants to move their piece  
 		String oldcolor;  // color of old chesspiece  
 		String newcolor; // color of new chesspiece  
-		boolean pawcheck; //check illegal move of pawn if needed 
+		boolean pawcheck=false; //check illegal move of pawn if needed  
+		boolean rookcheck=false; //check illegal move of rook if needed  
+		boolean bishopcheck=false; //check illegal bishop if needed 
 		
 		if (Character.isUpperCase(oldposcol)) { //Checks to see if the oldpositioncolumn is uppercase 
 			
@@ -502,7 +504,7 @@ public class Chess {
 			if (oldcolor.equalsIgnoreCase("W")) { // black player attempting to move their opponent player 
 				return true; 
 			}
-		}
+		} 
 		
 		
 		if (newpiece!=null) { //there is a chesspiece in the new position . 
@@ -517,43 +519,437 @@ public class Chess {
 		
 		if (oldpiece.getType().equalsIgnoreCase("P")) { // check for illegal move of pawn 
 			
-			pawcheck=illegalcheckpawn(oldposrow, oldposindexcol, newposrow, newposindexcol, count);  
+			if (whiteturn) { //white's turn to check 
+				pawcheck=illegalcheckwhitepawn(oldposrow, oldposindexcol, newposrow, newposindexcol, count);  
+				
+				if (pawcheck) {  // if a white player make an illegal pawn move 
+					return true; 
+				}
+			} 
 			
-			if (pawcheck) { 
+			else { // black's turn to check 
+				
+				pawcheck=illegalcheckblackpawn(oldposrow, oldposindexcol, newposrow, newposindexcol, count);  
+				
+				if (pawcheck) {  // if a black player made an illegal pawn move 
+					return true; 
+				}
+			}
+		} 
+		
+		if (oldpiece.getType().equalsIgnoreCase("R")) { // check the illegal move of rook  {
+			
+			rookcheck=illegalrook(oldposrow, oldposindexcol, newposrow, newposindexcol, count); 
+			
+			if (rookcheck) { 
 				return true; 
 			}
-		}
+		}  
+		
+		if (oldpiece.getType().equalsIgnoreCase("B")) { // check the illegal move of bishop  {
+			
+			bishopcheck=illegalbishop(oldposrow, oldposindexcol, newposrow, newposindexcol, count); 
+			
+			if (bishopcheck) { 
+				return true; 
+			}
+		}  
+		
 		
 		return false;
 	} 
 	
-	public static boolean illegalcheckpawn (int oldposrow, int oldposindexcol, int newposrow, int newposindexcol, int count) { 
+	public static boolean illegalcheckwhitepawn (int oldposrow, int oldposindexcol, int newposrow, int newposindexcol, int count) { 
 		
 		//pawns are allowed to move up the same file twice as long as its the first move  
 		int checkpieceindex=0; //check to see if there are any chesspiece ahead of pond by one space. 
-		int difference=0; //difference between newpiece and oldpiece 
+		int differencerows=0; //difference between newpiece rows and oldpiece rows for testing  
+		int differencecolumns=0; // difference between newpiece columns and oldpiece 
+		int checking=0; //checking to see the piece in front of pawn has a piece or not on the first try. 
+		 
 		
-		difference=newposrow-oldposrow;  
+		differencerows=newposrow-oldposrow;  
+		differencecolumns=newposindexcol-oldposindexcol; 
 		
-		if (difference>2) { // if a player attempets to move more than two spots  
+		if (differencerows<-2 || differencerows>-1) { // if a player attempets to move more than two spots  
+			return true; 
+		} 
+		
+		if (differencecolumns>1 || differencecolumns<-1) { // if a player attemps to move a a pawn diagonal more then two spots 
+
 			return true; 
 		}
 		
-		else if (difference==2) { 
+		if (differencerows==-2) {  
+			checking=oldposrow-1; 
 			
-			if (count!=0) { // player attempts to move two spaces after at least one turn after game started.  
+			if (oldposrow!=6) { // player attempts to move two spaces after at least one turn after game started.  
 				return true; 
-			} 
-			else {//count is 0 
+			}  
+			
+			if (differencecolumns!=0) { //player attemps to move two spaces diagonal=illegal 
+				return true; 
+			}
+			else {//count is 0 which means player's first attempt  
 				
-				if (oldposindexcol!=newposindexcol) { //player attempts to move two spaces to another file which is not allowed 
+				if (chessboard[checking][oldposindexcol].getPiece()!=null) { // if there is a piece in one space in from of pawn 
+					return true; 
+				}
+			}
+		} 
+		
+		if (differencerows==-1) { // white player moves forward one space or diagonal 
+			
+			if (differencecolumns!=0) { // white player does not move forward by one space 
+				
+				if (chessboard[newposrow][newposindexcol].getPiece()!=null) { // white player moves a diagonal spot with no piece in possession=illegal 
 					return true; 
 				}
 			}
 		}
 		
 		return false;
-	}
+	} 
+	
+	public static boolean illegalcheckblackpawn (int oldposrow, int oldposindexcol, int newposrow, int newposindexcol, int count) { 
+		
+		int columndifference=0; //difference between old column position and new column position  
+		int rowsdifference=0; //difference between   
+		columndifference=newposindexcol-oldposindexcol;  
+		rowsdifference=oldposrow-newposrow;  
+		
+		if (rowsdifference<1 || rowsdifference>2) { 
+			return true;  
+		}  
+		if (columndifference>2 || columndifference<-1) { 
+			return true; 
+		}  
+		if (rowsdifference==2) {  
+			
+			if (oldposrow!=1) { 
+				return true; 
+			}
+			
+			if (oldposindexcol!=newposindexcol) { 
+				return true; 
+			} 
+			if (chessboard[oldposrow+1][oldposindexcol].getPiece()!=null) { 
+				return true; 
+			}
+		} 
+		
+		if (rowsdifference==1) { 
+			
+			if (columndifference<-1 ||columndifference>1) { 
+				return true; 
+			}  
+			
+			if (columndifference==-1 || columndifference==1) { 
+				
+				if (chessboard[newposrow][newposindexcol].getPiece()==null) { 
+					return true; 
+				}
+			}  
+		}
+		return false;
+	} 
+	
+	public static boolean illegalrook (int oldposrow, int oldposindexcol, int newposrow, int newposindexcol, int count) { 
+		
+		if (oldposrow!=newposrow) { 
+			
+			if (oldposindexcol!=newposindexcol) { 
+				return true; 
+			}
+		} 
+		
+		// check the path to ensure there are no other pieces around  
+		
+		if (oldposrow==newposrow) { // player decides to move rook across 
+			
+			if (newposindexcol<oldposindexcol) { // moving to left of old position 
+				
+				for (int i=newposindexcol; i<oldposindexcol; i++) { // check to see if there are any pieces from new position to old position 
+					
+					if (chessboard[oldposrow][i].getPiece()!=null) { // if there is a piece in the path, illegal 
+						
+						return true; 
+					}
+				}
+			} 
+			
+			else { // trying to move oldpiece to the right 
+				
+				for (int j=newposindexcol; j>oldposindexcol; j--) { // check to see if there are any pieces from new position to old position 
+					
+					if (chessboard[oldposrow][j].getPiece()!=null) { // if there is a piece in the path, illegal 
+						
+						return true; 
+					}
+				}
+			}
+		} 
+		
+		
+		if (newposindexcol==oldposindexcol) { // player trying to move the rook up and down  
+			
+			if (newposrow<oldposrow) {  // rook  trying to move up
+				
+				for (int k=newposrow; k<oldposrow; k++) { // check to see if there are any pieces from new position to old position 
+					
+					if (chessboard[k][oldposindexcol].getPiece()!=null) { // if there is a piece in the path, illegal 
+						
+						return true; 
+					} 
+					
+				}
+			} 
+			else { // rook trying to move down 
+				
+				for (int l=newposrow; l<oldposrow; l--) { // check to see if there are any pieces from new position to old position 
+					
+					if (chessboard[l][oldposindexcol].getPiece()!=null) { // if there is a piece in the path, illegal 
+						
+						return true; 
+					} 
+					
+				}
+			}
+		}
+		
+		return false; 
+	} 
+	
+	public static boolean illegalbishop (int oldposrow, int oldposindexcol, int newposrow, int newposindexcol, int count) { 
+		
+		int rowmove=0; //  difference between newpiece row and oldpiece row 
+		int colmove=0;  // difference oldpiece column and newpiece column  
+		int checkingrow=0; 
+		int checkingcol=0; 
+		
+		
+		rowmove=oldposrow-newposrow; // calculate the difference oldpiece row and newpiece row  
+		colmove=oldposindexcol-newposindexcol; // calculate the difference between oldpiece column and newpiece column   
+	
+		rowmove=Math.abs(rowmove); 
+		colmove=Math.abs(colmove);  
+		
+		if (rowmove!=colmove) { // check for diagonal 
+			return true; 
+		} 
+		
+		if (newposindexcol>oldposindexcol) { // diagonal happened on the right side of oldpiece  
+			
+			if (newposrow<oldposrow) {  
+				checkingrow=oldposrow-1; 
+				checkingcol=oldposindexcol+1;
+				
+				while (checkingrow!=newposrow+1 && checkingcol!=newposindexcol) { 
+					
+					if (chessboard[checkingrow][checkingcol].getPiece()!=null) { 
+						return true; 
+					} 
+					
+					checkingrow--; 
+					checkingcol++;
+				}
+			} 
+			
+			else {  
+				checkingrow=oldposrow+1; 
+				checkingcol=oldposindexcol-1;
+				
+				while (checkingrow!=newposrow && checkingcol!=newposindexcol) { 
+					
+					if (chessboard[checkingrow][checkingcol].getPiece()!=null) { 
+						return true; 
+					} 
+					
+					checkingrow++; 
+					checkingcol++;
+				}
+
+			}
+		} 
+		
+		else { //diagonal happened on the left side of the oldpiece 
+			
+			if (newposrow<oldposrow) {  
+				checkingrow=oldposrow-1; 
+				checkingcol=oldposindexcol-1;
+				
+				while (checkingrow!=newposrow && checkingcol!=newposindexcol) { 
+					
+					if (chessboard[checkingrow][checkingcol].getPiece()!=null) { 
+						return true; 
+					} 
+					
+					checkingrow--; 
+					checkingcol--;
+				}
+			} 
+			
+			else { 
+				
+				checkingrow=oldposrow+1; 
+				checkingcol=oldposindexcol-1;
+				while (checkingrow!=newposrow && checkingcol!=newposindexcol) { 
+					
+					if (chessboard[checkingrow][checkingcol].getPiece()!=null) { 
+						return true; 
+					} 
+					
+					checkingrow++; 
+					checkingcol--;
+				}
+			}
+			
+		}
+		
+		return false; 
+	} 
+	
+	public static boolean illegalqueen (int oldposrow, int oldposindexcol, int newposrow, int newposindexcol, int count) { 
+		
+		int checkcolumn=0;  
+		int checkrows=0;  
+		int diffrows=oldposrow-newposrow; 
+		int diffcolumns=oldposindexcol-newposindexcol;  
+		int checkdiagonalrows=0; 
+		int checkdiagonalcols=0; 
+		diffrows=Math.abs(diffrows); 
+		diffcolumns=Math.abs(diffcolumns); 
+		
+		if (oldposrow==newposrow) { //  queen plans on moving across the board  
+			
+			if (newposrow<oldposrow) { //newposition is to the left of oldposition  
+				checkcolumn=newposindexcol+1; 
+				
+				for (int m=checkcolumn; m<oldposindexcol; m++) { // check the path to see if there is another path 
+					
+					if (chessboard[oldposrow][checkcolumn].getPiece()!=null) { // there is a piece in the path 
+						return true; 
+					}  
+				} 
+				return false; 
+			} 
+			
+			else {  // newpiece is to the right of oldpiece  
+				checkcolumn=newposindexcol-1; 
+				
+				for (int n=checkcolumn; n>oldposindexcol; n--) { // check the path to see if there is another path 
+					
+					if (chessboard[oldposrow][n].getPiece()!=null) { // there is a piece in the path 
+						return true; 
+					}  
+				} 
+				return false; 
+			}
+		} 
+		
+		else if (oldposindexcol==newposindexcol) { //  queen moves up and down the board  
+			 
+				if (newposrow<oldposrow) {  
+					checkrows=newposrow+1;  
+					
+					for (int o=checkrows; o<oldposrow; o++) { // check the path to see if there is another path 
+					
+						if (chessboard[o][oldposindexcol].getPiece()!=null) { // there is a piece in the path 
+							return true; 
+						}  
+					} 
+					return false; 
+				} 
+				else { 
+					checkrows=newposrow-1; 
+					
+					for (int p=checkrows; p>oldposindexcol; p--) { // check the path to see if there is another path 
+					
+						if (chessboard[p][oldposindexcol].getPiece()!=null) { // there is a piece in the path 
+							return true; 
+						}  
+					} 
+					return false; 
+				}
+		
+		} 
+		// check for diagonals if there is one  
+	
+		else if (diffrows==diffcolumns) { // there is a diagonal 
+			
+			if (newposindexcol>oldposindexcol) { // diagonal to the right of oldpiece 
+				
+				if (newposrow<oldposrow) { //-+ for rows and columns. Diagonal to the right and up 
+					checkdiagonalrows=oldposrow-1;  
+					checkdiagonalcols=oldposindexcol+1;  
+					
+					while (checkdiagonalrows!=newposrow && checkdiagonalcols !=newposindexcol) { 
+						
+						if (chessboard[checkdiagonalrows][checkdiagonalcols].getPiece()!=null) { // there is a piece in the diagonal path 
+							return true; 
+						}    
+						checkdiagonalrows--; 
+						checkdiagonalcols++; 
+					} 
+					return false; 
+				} 
+				
+				else { // ++ for rows and columns. Diagonal to the right and down 
+					checkdiagonalrows=oldposrow+1;  
+					checkdiagonalcols=oldposindexcol+1;  
+					
+					while (checkdiagonalrows!=newposrow && checkdiagonalcols !=newposindexcol) { 
+						
+						if (chessboard[checkdiagonalrows][checkdiagonalcols].getPiece()!=null) { // there is a piece in the diagonal path 
+							return true; 
+						}    
+						checkdiagonalrows++; 
+						checkdiagonalcols++; 
+					} 
+					return false; 
+					
+				}
+			} 
+			
+			else { // diagonal to the left 
+				checkdiagonalrows=oldposrow-1;  
+				checkdiagonalcols=oldposindexcol-1;  
+				
+				if (newposindexcol<oldposindexcol) {// diagonal to the left and up 
+				
+					while (checkdiagonalrows!=newposrow && checkdiagonalcols !=newposindexcol) { 
+					
+						if (chessboard[checkdiagonalrows][checkdiagonalcols].getPiece()!=null) { // there is a piece in the diagonal path 
+							return true; 
+					}    
+					checkdiagonalrows--; 
+					checkdiagonalcols--; 
+					} 
+					return false; 
+				} 
+				
+				else { // diagonal to the left and down , +- 
+					checkdiagonalrows=oldposrow+1;  
+					checkdiagonalcols=oldposindexcol-1;
+					
+					while (checkdiagonalrows!=newposrow && checkdiagonalcols !=newposindexcol) { 
+						
+						if (chessboard[checkdiagonalrows][checkdiagonalcols].getPiece()!=null) { // there is a piece in the diagonal path 
+							return true; 
+					}    
+					checkdiagonalrows++; 
+					checkdiagonalcols--; 
+					} 
+					return false; 
+				}
+			}
+		} 
+		
+		else { // all other moves illegal 
+			
+			return true; 
+		}
+	} 
 }
 
 
